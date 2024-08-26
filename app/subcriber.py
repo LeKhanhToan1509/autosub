8e7e92031ba044b485a138cbdee37fe5b9263f4a
Trong Le <trongthptdqh@gmail.com>
2024-08-26 13:36:08 +0700

    refactor by trongle

--- app/subcriber.py
+++ app/subcriber.py
@@ -5,48 +5,63 @@ from pymongo import MongoClient
 import os
 import requests
 from dotenv import load_dotenv
+
+# Load env
 load_dotenv()
 
 DOMAIN = os.getenv("DOMAIN")
 MONGO_URL = os.getenv("MONGODB_CONNECTION")
 client = MongoClient(MONGO_URL)
-r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
+accounts_collection = client['Autosub_PTIT']['accounts']
 
+def is_account_valid(account, password):
+    url = f"{DOMAIN}/check-account?username={account}&password={password}"
+    response = requests.get(url)
+    return response.status_code == 200
 
+async def handle_redis_messages(redis_client):
+    pubsub = redis_client.pubsub()
+    pubsub.subscribe('addaccount', 'delaccount')
 
-async def listen_to_redis(r):
-    add = r.pubsub()
-    rm = r.pubsub()
-    add.subscribe('addaccount')
-    rm.subscribe('delaccount')
-    acc = client['Autosub_PTIT']['accounts']
     while True:
-        add_data = add.get_message()
-        rm_data = rm.get_message()
+        message = pubsub.get_message()
 
-        if add_data:
-            message = add_data['data']
-            if message != 1:
-                info = json.loads(message)
-                account = info['account']
-                password = info['password']
-                url = f"{DOMAIN}/check-account?username={account}&password={password}"
-                response = requests.request("GET", url)
-                if response.status_code == 200:
-                    print("Login success")
-                    if(acc.find_one({"account": account}) == None):
-                        acc.insert_one({"account": account, "password": password})
-        if rm_data:
-            message = rm_data['data']
-            if message != 1:  
-                account = message
-                if(acc.find_one({"account": account}) != None):
-                    acc.delete_one({"account": account})
-        await asyncio.sleep(0.1) 
+        if message and message['type'] == 'message':
+            channel = message['channel']
+            data = message['data']
+
+            if channel == 'addaccount':
+                await handle_add_account(data)
+            elif channel == 'delaccount':
+                await handle_delete_account(data)
+
+        await asyncio.sleep(0.1)
+
+async def handle_add_account(data):
+    try:
+        account_info = json.loads(data)
+        account = account_info['account']
+        password = account_info['password']
+
+        if is_account_valid(account, password):
+            print(f"Đăng nhập thành công cho tài khoản: {account}")
+            if not accounts_collection.find_one({"account": account}):
+                accounts_collection.insert_one({"account": account, "password": password})
+                print(f"Tài khoản {account} đã được thêm vào MongoDB")
+    except json.JSONDecodeError:
+        print("Không thể giải mã tin nhắn JSON")
+
+async def handle_delete_account(data):
+    account = data
+    if accounts_collection.find_one({"account": account}):
+        accounts_collection.delete_one({"account": account})
+        print(f"Tài khoản {account} đã được xóa khỏi MongoDB")
+
 
 def main():
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(listen_to_redis(r))
+    loop.run_until_complete(handle_redis_messages(redis_client))
 
 if __name__ == "__main__":
     main()
